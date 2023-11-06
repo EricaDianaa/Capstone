854dfe92e1325a66e6385c53ce0ea8e54db2d97b
Erica Diana <134380180+EricaDianaa@users.noreply.github.com>
2023-11-06 18:54:50 +0100

    Inizio Css pagina Login,Register,Home,RecuperoPassword

---
+++ Capstone/Content/JavaScriptLoginRegister.js
@@ -0,0 +1,6 @@
+﻿$(".info-item .btn").click(function () {
+    $(".container").toggleClass("log-in");
+});
+$(".container-form .btn").click(function () {
+    $(".container").addClass("active");
+})
